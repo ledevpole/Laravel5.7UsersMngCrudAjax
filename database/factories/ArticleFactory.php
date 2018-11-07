686afa1e5bb9a7d9d8714804c411112c991fa547
ledevpole <ledevpole@gmail.com>
2018-11-08 00:30:03 +0100

    Messages seeder and factory

--- database/factories/ArticleFactory.php
+++ database/factories/ArticleFactory.php
@@ -5,7 +5,7 @@ use Faker\Generator as Faker;
 $factory->define(App\Article::class, function (Faker $faker) {
     return [
     	'title' => $faker->sentence($nbWords = 6, $variableNbWords = true),
-    	'body'  => $faker->paragraph($nbSentences = 3, $variableNbSentences = true),
+    	'body'  => $faker->paragraph($nbSentences = 6, $variableNbSentences = true),
     	'user_id' => $faker->randomDigitNotNull,     
     ];
 });
--- database/seeds/MessagesTableSeeder.php
+++ database/seeds/MessagesTableSeeder.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Database\Seeder;
 
-class CommentsTableSeeder extends Seeder
+class MessagesTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
@@ -11,6 +11,6 @@ class CommentsTableSeeder extends Seeder
      */
     public function run()
     {
-        factory(App\Comment::class, 300)->create();
+        factory(App\Message::class, 30)->create();
     }
 }
